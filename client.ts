cc2e3f52a31320bcd0d5ee84ffea9b5c7c57905d
yashvirawal-86 <yashvirawal86@gmail.com>
2026-07-17 21:16:29 +0530

    Delete client.ts

--- client.ts
+++
@@ -1,8 +0,0 @@
-import { StartClient } from "@tanstack/react-start";
-import { createElement } from "react";
-import { hydrateRoot } from "react-dom/client";
-import { getRouter } from "./router";
-
-const router = getRouter();
-
-hydrateRoot(document, createElement(StartClient, { router }));
