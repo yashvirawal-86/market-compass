17055d2d41019682987b57d06f2619a056ea104e
yashvirawal-86 <yashvirawal86@gmail.com>
2026-07-18 15:43:02 +0530

    Delete src/client.tsx

--- src/client.tsx
+++
@@ -1,9 +0,0 @@
-import { createElement } from "react";
-import { createRoot } from "react-dom/client";
-import { getRouter } from "./router";
-import { RouterProvider } from "@tanstack/react-router";
-
-const router = getRouter();
-
-const container = document.getElementById("root") ?? document.body;
-createRoot(container).render(createElement(RouterProvider, { router }));
